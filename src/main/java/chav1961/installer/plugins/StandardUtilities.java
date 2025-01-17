73c509b4feb0baee1cfba7b9f49d397074b3de2b
chav1961 <chav1961@mail.ru>
2025-01-17 13:52:12 +0500

    DAily commit

--- src/main/java/chav1961/installer/plugins/StandardUtilities.java
+++ src/main/java/chav1961/installer/plugins/StandardUtilities.java
@@ -18,6 +18,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.security.spec.NamedParameterSpec;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -34,6 +35,7 @@ import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.InputVerifier;
@@ -45,8 +47,15 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SpringLayout;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableModel;
 import javax.swing.text.JTextComponent;
 
 import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
@@ -67,6 +76,7 @@ import chav1961.purelib.basic.exceptions.LocalizationException;
 import chav1961.purelib.basic.interfaces.LoggerFacade.Severity;
 import chav1961.purelib.i18n.interfaces.Localizer;
 import chav1961.purelib.i18n.interfaces.Localizer.LocaleChangeListener;
+import chav1961.purelib.model.FieldFormat;
 import chav1961.purelib.ui.swing.SwingUtils;
 import chav1961.purelib.ui.swing.useful.JLocalizedOptionPane;
 import chav1961.purelib.ui.swing.useful.LabelledLayout;
@@ -99,6 +109,17 @@ public class StandardUtilities {
 	private static final String		STD_JDBC_SETTINGS_SCREEN_FIELD_USER = "std.jdbc.settings.screen.field.user";
 	private static final String		STD_JDBC_SETTINGS_SCREEN_FIELD_PASSWORD = "std.jdbc.settings.screen.field.password";
 	private static final String		STD_JDBC_SETTINGS_SCREEN_BUTTON_TEST = "std.jdbc.settings.screen.button.test";
+
+	private static final String		NAME_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR = "targetDir";
+	private static final String		NAME_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED = "freeMemoryRequired";
+	private static final String		NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_MUST_BE_EMPTY = "dirMustBeEmpty";
+	private static final String		NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_WILL_BE_CLEANED = "dirWillBeCleaned";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_PREAMBLE = "std.select.target.directory.screen.preamble";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR = "std.select.target.directory.screen.field.target.dir";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED = "std.select.target.directory.screen.field.free.memory.required";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_FIELD_MUST_BE_EMPTY = "std.select.target.directory.screen.field.must.be.empty";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_FIELD_WILL_BE_CLEANED = "std.select.target.directory.screen.field.will.be.cleaned";
+	private static final String		STD_SELECT_TARGET_DIR_SCREEN_FIELD_AVAILABLE_ROOTS = "std.select.target.directory.screen.field.available.roots";
 	
 	private static final StandardUtilities	instance = new StandardUtilities();
 
@@ -303,6 +324,39 @@ public class StandardUtilities {
 		return null;
 	}
 
+	/*
+	 * {"targetDir":"path","freeMemoryRequired":memoryInBytes,"dirMustBeEmpty":{true/false},"dirWillBeCleaned":{true/false}}
+	 */
+	public JComponent getSelectTargetDirScreen(final ScriptObjectMirror parm) {
+		final Wizard					w = (Wizard)Application.INSTALLATION_CONTEXT.get(Application.CTX_WIZARD);
+		final InstallationService		service = (InstallationService)Application.INSTALLATION_CONTEXT.get(Application.CTX_SERVICE);
+		final List<ContentKeeper<?>>	options = new ArrayList<>();
+
+		extractValue2TextField(parm, NAME_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR, STD_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR, options);
+		extractValue2TextField(parm, NAME_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED, STD_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED, options);
+		extractValue2CheckBox(parm, NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_MUST_BE_EMPTY, STD_SELECT_TARGET_DIR_SCREEN_FIELD_MUST_BE_EMPTY, options);
+		extractValue2CheckBox(parm, NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_WILL_BE_CLEANED, STD_SELECT_TARGET_DIR_SCREEN_FIELD_WILL_BE_CLEANED, options);
+
+		return new SelectTargetDirContent(w.getLocalizer(),
+				new ContentKeeper<JEditorPane>(
+					"text",
+					InternalUtils.createEditorPane(),
+					Utils.mkMap(new NamedValue(KEY_TEXT, STD_SELECT_TARGET_DIR_SCREEN_PREAMBLE),
+						new NamedValue(KEY_PARAMS, new Supplier[] {
+								()->service.getLocalizer().getValue(service.getProductName()),
+								()->service.getLocalizer().getValue(service.getProductDescription()),
+								()->service.getProductVersion().toString(),
+								()->service.getLocalizer().getValue(service.getProductVendor().toString()),
+								()->service.getProductSite()
+							})
+					)
+				),
+				options
+		);
+	}	
+	
+	
+	
 	private static String toString(final Object value) {
 		if (value == null) {
 			return "";
@@ -680,22 +734,271 @@ public class StandardUtilities {
 		}
 	}	
 
-	
-	private static class ParameterSubstitutor implements SubstitutionSource {
-		private final Object[]	parameters;
+	public static class SelectTargetDirContent extends JPanel implements LocaleChangeListener, OptionsKeeper<SelectTargetDirContent.Options>, OptionsChecker {
+		private static final long serialVersionUID = 423994106219046519L;
+
+		private final Localizer					localizer;
+		private final ContentKeeper<?>			preamble;
+		private final List<JLabel>				labels = new ArrayList<>();
+		private final List<ContentKeeper<?>>	options;
+		private final JLabel					sizeTitle = new JLabel();
+		private final SizeTableModel			tableModel;
+
+		private SelectTargetDirContent(final Localizer localizer, final ContentKeeper<?> preamble, final List<ContentKeeper<?>> options) {
+			super(new BorderLayout(10, 10));
+			this.localizer = localizer;
+			this.preamble = preamble;
+			this.options = options;
+			final SpringLayout	sl = new SpringLayout();
+			final JPanel	totalPage = new JPanel(sl);
+			final JPanel	optList = new JPanel(new LabelledLayout());
+			final JPanel	sizeList = new JPanel();
+			final BoxLayout	bl = new BoxLayout(sizeList, BoxLayout.PAGE_AXIS);
+			
+			add((JComponent)preamble.content, BorderLayout.NORTH);
+			for(ContentKeeper<?> item : options) {
+				final JLabel		label = new JLabel();
+				final JComponent	component = (JComponent)item.content;
+
+				switch (item.name) {
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED	:
+						((JTextField)component).setEditable(false);
+						break;
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_MUST_BE_EMPTY		:
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_WILL_BE_CLEANED	:
+						label.setEnabled(false);
+						component.setEnabled(false);
+						break;
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR				:
+					default:
+						component.setInputVerifier(new InputVerifier() {
+							@Override
+							public boolean verify(final JComponent input) {
+								return verifyOptions(item.name, input);
+							}
+						});
+						break;
+				}
+				optList.add(label, LabelledLayout.LABEL_AREA);
+				optList.add(component, LabelledLayout.CONTENT_AREA);
+				labels.add(label);
+			}
+			final List<NamedValue<Long>>	roots = new ArrayList<>();
+			
+			for(File item : File.listRoots()) {
+				roots.add(new NamedValue<>(item.getAbsolutePath(), item.getFreeSpace()));
+			}
+			this.tableModel = new SizeTableModel(localizer, roots);
+			final JTable	table = new JTable(tableModel);
+			
+			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			table.setColumnSelectionAllowed(false);
+			table.setDefaultRenderer(Long.class, SwingUtils.getCellRenderer(long.class, new FieldFormat(long.class), TableCellRenderer.class));
+			
+			sizeList.setLayout(bl);
+			sizeList.add(sizeTitle);
+			sizeList.add(new JScrollPane(table));
+			
+			totalPage.add(optList);
+			totalPage.add(sizeList);
+			sl.putConstraint(SpringLayout.NORTH, optList, 0, SpringLayout.NORTH, totalPage);
+			sl.putConstraint(SpringLayout.WEST, optList, 0, SpringLayout.WEST, totalPage);
+			sl.putConstraint(SpringLayout.EAST, optList, 0, SpringLayout.EAST, totalPage);
+			sl.putConstraint(SpringLayout.NORTH, sizeList, 20, SpringLayout.SOUTH, optList);
+			sl.putConstraint(SpringLayout.WEST, optList, 0, SpringLayout.WEST, totalPage);
+			sl.putConstraint(SpringLayout.EAST, optList, 0, SpringLayout.EAST, totalPage);
+			
+			add(totalPage, BorderLayout.CENTER);
+			fillLocalizedStrings();
+		}
 		
-		private ParameterSubstitutor(final Object... parameters) {
-			this.parameters = parameters;
+		@Override
+		public void localeChanged(final Locale oldLocale, final Locale newLocale) throws LocalizationException {
+			fillLocalizedStrings();
 		}
 
 		@Override
-		public String getValue(final String key) {
-			// TODO Auto-generated method stub
-			return null;
+		public boolean isNextActionAvailable() {
+			final Options	opts = getOptions();
+			
+			return (!opts.targetDir.exists() || opts.targetDir.isDirectory() && opts.targetDir.canWrite());
+		}
+
+		@Override
+		public boolean checkOptions() {
+			final Options	opts = getOptions();
+			
+			if (opts.targetDir.getFreeSpace() < opts.freeMemoryRequired) {
+				SwingUtils.getNearestLogger(this).message(Severity.warning, "No enough space on device");
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+
+		@Override
+		public Options getOptions() {
+			String	targetDir = "";
+			long	memory = 0;
+			boolean	mustBeEmpty = true;
+			boolean	willBeCleaned = true;
+			
+			for (ContentKeeper<?> item : options) {
+				switch (item.name) {
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR				:
+						targetDir = ((JTextField)item.content).getText();
+						break;
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_FREE_MEMORY_REQUIRED	:
+						memory = Long.valueOf(((JTextField)item.content).getText());
+						break;
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_MUST_BE_EMPTY		:
+						mustBeEmpty = ((JCheckBox)item.content).isSelected();
+						break;
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_DIR_WILL_BE_CLEANED	:
+						willBeCleaned = ((JCheckBox)item.content).isSelected();
+						break;
+					default :
+						throw new UnsupportedOperationException("Item name ["+item.name+"] is not supported yet");
+				}
+			}
+			return new Options(new File(targetDir), memory, mustBeEmpty, willBeCleaned);
+		}
+
+		private boolean verifyOptions(final String currentOption, final JComponent input) {
+			String	value = "";
+
+			try {
+				switch (currentOption) {
+					case NAME_SELECT_TARGET_DIR_SCREEN_FIELD_TARGET_DIR			:
+						value = ((JTextComponent)input).getText().trim();
+						
+						if (Utils.checkEmptyOrNullString(value)) {
+							SwingUtils.getNearestLogger(input).message(Severity.warning, "empty traget directory name!");
+							return false;
+						}
+						else {
+							final File	f = new File(value);
+							
+							if (!f.exists() || !f.isFile() || !f.canRead()) {
+								SwingUtils.getNearestLogger(input).message(Severity.warning, "driver name [] not exists!");
+								return false;
+							}
+							else {
+								try(final URLClassLoader	loader = new URLClassLoader(new URL[] {f.toURI().toURL()})) {
+									
+									for(Driver item : ServiceLoader.load(java.sql.Driver.class, loader)) {
+										return true;
+									}
+									SwingUtils.getNearestLogger(input).message(Severity.warning, "driver name [] not exists!");
+									return false;
+								} catch (IOException e) {
+									SwingUtils.getNearestLogger(input).message(Severity.warning, "driver name [] not exists!");
+									return false;
+								}
+							}
+						}
+					default :
+						throw new UnsupportedOperationException("Option ["+currentOption+"] is not supported yet");
+				}
+			} finally {
+				sendRefresh(this);
+			}
+		}
+
+		private void fillLocalizedStrings() {
+			((JEditorPane)preamble.content).setText(InternalUtils.loadHtml(localizer, preamble.attributes.get(KEY_TEXT).toString()));
+			for (int index = 0; index < options.size(); index++) {
+				labels.get(index).setText(localizer.getValue(options.get(index).attributes.get(KEY_TEXT).toString()));
+			}
+			sizeTitle.setText(localizer.getValue(STD_SELECT_TARGET_DIR_SCREEN_FIELD_AVAILABLE_ROOTS));
+		}
+
+
+		public static class Options {
+			public final File		targetDir;
+			public final long		freeMemoryRequired;
+			public final boolean 	dirMustBeEmpty;
+			public final boolean 	dirWillBeCleaned;
+			
+			public Options(File targetDir, long freeMemoryRequired, boolean dirMustBeEmpty, boolean dirWillBeCleaned) {
+				this.targetDir = targetDir;
+				this.freeMemoryRequired = freeMemoryRequired;
+				this.dirMustBeEmpty = dirMustBeEmpty;
+				this.dirWillBeCleaned = dirWillBeCleaned;
+			}
+
+			@Override
+			public String toString() {
+				return "Options [targetDir=" + targetDir + ", freeMemoryRequired=" + freeMemoryRequired + ", dirMustBeEmpty=" + dirMustBeEmpty + ", dirWillBeCleaned=" + dirWillBeCleaned + "]";
+			}
+		}
+		
+		private static class SizeTableModel extends DefaultTableModel {
+			private static final long 		serialVersionUID = 1L;			
+			private static final String		STD_TABLE_ROOT_COLUMN_NAME = "std.select.target.directory.screen.field.size.column.root"; 
+			private static final String		STD_TABLE_SIZE_COLUMN_NAME = "std.select.target.directory.screen.field.size.column.size"; 
+			
+			private final Localizer					localizer;
+			private final List<NamedValue<Long>> 	content;
+			
+			private SizeTableModel(final Localizer localizer, final List<NamedValue<Long>> content) {
+				this.localizer = localizer;
+				this.content = content;
+			}
+			
+			@Override
+			public int getRowCount() {
+				return content == null ? 0 : content.size();
+			}
+
+			@Override
+			public int getColumnCount() {
+				return 2;
+			}
+
+			@Override
+			public String getColumnName(final int columnIndex) {
+				switch (columnIndex) {
+					case 0 :
+						return localizer.getValue(STD_TABLE_ROOT_COLUMN_NAME);
+					case 1 :
+						return localizer.getValue(STD_TABLE_SIZE_COLUMN_NAME);
+					default :
+						return "?";
+				}
+			}
+
+			@Override
+			public Class<?> getColumnClass(final int columnIndex) {
+				switch (columnIndex) {
+					case 0 :
+						return String.class;
+					case 1 :
+						return Long.class;
+					default :
+						return Object.class;
+				}
+			}
+
+			@Override
+			public boolean isCellEditable(final int rowIndex, final int columnIndex) {
+				return false;
+			}
+
+			@Override
+			public Object getValueAt(final int rowIndex, final int columnIndex) {
+				switch (columnIndex) {
+					case 0 :
+						return content.get(rowIndex).getName();
+					case 1 :
+						return content.get(rowIndex).getValue();
+					default :
+						return null;
+				}
+			}
 		}
 	}
-	
-	
 	
 	public static StandardUtilities getInstance() {
 		return instance;
--- src/main/java/chav1961/installer/products/install.js
+++ src/main/java/chav1961/installer/products/install.js
@@ -1,8 +1,6 @@
 function install() {
 	var properties = STD.loadOptions("c:/tmp/assa.props", false);
-	
 	var jdbcOptions = {driver:"",connString:"",user:"",password:""};
-//	var jdbcOptions = {driver:"C:/svns/dev/trunk/postgresql-42.4.1.jar",connString:"jdbc:postgresql://localhost:5432/postgres",user:"postgres",password:"postgres"};
 	
 	STD.extractOptions(properties, jdbcOptions);
 	var screen = STD.getJdbcSettingsScreen(jdbcOptions);
@@ -37,7 +35,39 @@ function install() {
 				result = true;
 				return result;		
 	}
-	
+
+	screen = STD.getSelectTargetDirScreen({targetDir:"",freeMemoryRequired:1000,dirMustBeEmpty:true,dirWillBeCleaned:true});
+	var targetDirOptions;
+
+	switch (
+		WIZARD.pushContent(
+			"helloworld.last", 
+			SERVICE.getLocalizer(), 
+			SERVICE.getProductName(), 
+			"helloworld.install.complete", 
+			screen, 
+			true, 
+			function(c){return screen.checkOptions();},
+			function(c){return screen.isNextActionAvailable();}).name()
+		) {
+			case 'PREVIOUS':
+				WIZARD.popContent('_sp_');		
+				result = false;
+				break;		
+			case 'COMPLETE':
+				print(targetDirOptions = screen.getOptions());
+				result = true;
+				break;		
+			case 'CANCEL':
+				print("Cancel");
+				result = true;
+				break;		
+			case 'CANCEL_WITH_KEEP_SETTINGS':
+				print("Save and Cancel");
+				result = true;
+				break;		
+	}
+		
 	screen = STD.getLastScreen({showReadme:true});
 	var lastScreenOptions;
 	
